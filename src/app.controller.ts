b68c33e5a97d166b73cf0701fb059f26fb9a9d61
Sayand Sathish <sayandcode@gmail.com>
2024-10-14 08:14:55 +0530

    feat: Add index route
    We added handlebars templating, remove the unused AppService
    and render an index/home route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Render("index")
+  getHome() {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestApplication, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { join, resolve } from "node:path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+  app.setBaseViewsDir(resolve(__dirname, "../views"));
+  app.setViewEngine("hbs");
   await app.listen(3000);
 }
 bootstrap();
